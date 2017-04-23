dd83d9a10e62fd1ececd421c3d5c214a64eb5d3c
Depuydt Marion <marion.depuydt@etu.utc.fr>
2017-04-23 11:14:13 +0200

    Add info message to markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,7 +15,7 @@ export class MapComponent implements OnInit {
   public latitude: number;
   public longitude: number;
   public zoom: number;
-  public locations: Array<{latitude: number, longitude: number}>;
+  public locations: Array<{latitude: number, longitude: number, title: string, photo: string, creationDate: string}>;
 
   constructor(private articleService: ArticleService) {
   }
@@ -24,13 +24,19 @@ export class MapComponent implements OnInit {
     this.latitude = 0.0000;
     this.longitude = 0.0000;
     this.zoom = 2;
-    this.locations = Array<{latitude: number, longitude: number}>();
+    this.locations = Array<{latitude: number, longitude: number, title: string, photo: string, creationDate: string}>();
 
     this.articleService.getArticles().then(
       articles => {
         articles.forEach(
           (art) => {
-            this.locations.push({'latitude': art.location.latitude, 'longitude': art.location.longitude})
+            this.locations.push({
+              'latitude': art.location.latitude,
+              'longitude': art.location.longitude,
+              'title': art.title,
+              'photo': art.photos[0],
+              'creationDate': art.creationDate
+            })
           }
         );
         console.log(this.locations)
